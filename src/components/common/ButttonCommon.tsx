99954975ea4d7cd940e088b74843ae4574fd054a
Rupesh Prajapati <rupesh@crmtoolbox.io>
2024-08-01 17:35:58 +0530

    complete login page

--- src/components/common/ButttonCommon.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const ButttonCommon = ({ BtnName }: any) => {
-  return (
-    <button className="text-white bg-greenPrimary hover:brightness-110 px-3 py-[10px] font-normal text-base w-full rounded   mt-9 transition-all duration-300">
-      {BtnName}
-    </button>
-  );
-};
