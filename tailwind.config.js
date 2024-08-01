6037d4482174e548906ba57eccfaf2f34a60535f
khushbu.patel <kushbu.patel@esparkbizmail.com>
2024-08-01 13:29:39 +0530

    login page design created

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx,html,css}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+          greenPrimary : "#09A17A",
+          blackPrimary : "#242425",
+          grayText: "#6C778B",
+          grayLightBody:"#97A0B2",
+          inputAuthBg: "#EAEDF3",
+      },
+      backgroundImage:{
+        authPattern: "url('/images/bgPatternAuth.png');",
+      },
+      fontFamily:{
+        jost : "Jost",
+      }
+    },
   },
   plugins: [],
 };
