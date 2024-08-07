0ed1c414af45f5304dd64886d0474f93d226cb7c
ian-aguilar <josemelend.dev@gmail.com>
2024-08-07 18:12:35 +0530

    build issue resolve

--- src/modules/Auth/components/RequiresAuth.tsx
+++ src/modules/Auth/components/RequiresAuth.tsx
@@ -1,13 +1,13 @@
 // ** packages **
 import { Suspense } from "react";
-import { useSelector } from "react-redux";
-import { Navigate, Outlet } from "react-router-dom";
+// import { useSelector } from "react-redux";
+import { Outlet } from "react-router-dom";
 
 // ** redux **
-import { getAuth } from "../../../redux/slices/authSlice";
+// import { getAuth } from "../../../redux/slices/authSlice";
 
 const RequiresAuth = () => {
-  const { isAuthenticated } = useSelector(getAuth);
+  // const { isAuthenticated } = useSelector(getAuth);
 
   // /* Not Logged In */
   // if (!isAuthenticated) {
