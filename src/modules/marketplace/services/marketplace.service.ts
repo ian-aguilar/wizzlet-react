9af26039d8d5a7e8b66390c45e59e36ca875899c
omgajipara23 <gajiparaom40@gmail.com>
2024-08-08 12:38:08 +0530

    marketplace listing api added

---
+++ src/modules/marketplace/services/marketplace.service.ts
@@ -0,0 +1,15 @@
+import { useAxiosGet } from "@/hooks/useAxios";
+
+const AUTH_API_BASE_PATH = "/marketplace";
+
+//  ** Get All Marketplace Listing **
+export const useMarketplaceListingAPI = () => {
+  // ** custom Hooks **
+  const [callApi, { isLoading, isError, isSuccess }] = useAxiosGet();
+
+  const getMarketplaceListingAPI = async (data?: object) => {
+    return callApi(`${AUTH_API_BASE_PATH}/all`, data);
+  };
+
+  return { getMarketplaceListingAPI, isLoading, isError, isSuccess };
+};
