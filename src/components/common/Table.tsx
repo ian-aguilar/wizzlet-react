2823d4f29698e42fcc54b4cb7e2dd119177b1327
ian-aguilar <josemelend.dev@gmail.com>
2024-08-08 17:53:57 +0530

    table added for use listing

---
+++ src/components/common/Table.tsx
@@ -0,0 +1,86 @@
+// Packages
+import { debounce } from "lodash";
+import { useCallback, useEffect, useState } from "react";
+import DataTable, { SortOrder, TableColumn } from "react-data-table-component";
+
+// types
+import { ISort, ITableProps } from "@/components/common/types/table";
+
+function Table<T>({ getData, loading, columns }: ITableProps<T>) {
+  const [data, setData] = useState<T[]>([]);
+  const [limit, setLimit] = useState(10);
+  const [totalRows, setTotalRows] = useState(0);
+  const [page, setPage] = useState(1);
+  const [sort, setSort] = useState<ISort>({});
+  const [search, setSearch] = useState<string>();
+
+  const fetchData = async (
+    page: number,
+    rowsPerPage: number,
+    sort: ISort,
+    search?: string
+  ) => {
+    const { data, totalRecord } = await getData({
+      page,
+      rowsPerPage,
+      sortField: sort.field,
+      sortDirection: sort.direction,
+      search,
+    });
+    setData(data);
+    setTotalRows(totalRecord);
+  };
+
+  const handlePageChange = (page: number) => setPage(page);
+
+  const handlePerRowsChange = (limit: number, page: number) => {
+    setLimit(limit);
+    setPage(page);
+  };
+
+  const handleSort = (
+    selectedColumn: TableColumn<T>,
+    sortDirection: SortOrder
+  ) => {
+    setSort({
+      direction: sortDirection,
+      field: selectedColumn.sortField,
+    });
+    setPage(1);
+  };
+
+  const setSearchValue = useCallback(
+    debounce((e) => {
+      setSearch(e.target.value);
+      setPage(1);
+    }, 500),
+    []
+  );
+
+  useEffect(() => {
+    fetchData(page, limit, sort, search);
+  }, [page, limit, sort, search]);
+
+  return (
+    <>
+      <input type="text" onChange={setSearchValue} />
+      <DataTable<T>
+        className="dataTable"
+        columns={columns}
+        data={data}
+        progressPending={loading}
+        pagination
+        paginationServer
+        paginationTotalRows={totalRows}
+        onChangeRowsPerPage={handlePerRowsChange}
+        onChangePage={handlePageChange}
+        progressComponent={<div>Loading</div>}
+        // sortIcon={sortIcon}
+        onSort={handleSort}
+        noDataComponent={<>There are no records to display!!!!</>}
+      />
+    </>
+  );
+}
+
+export default Table;
---
+++ src/helper/index.ts
@@ -0,0 +1,73 @@
+import axios from "axios";
+import { MutableRefObject } from "react";
+import { FieldErrors } from "react-hook-form";
+
+export const stringToBoolean = (value: any) => {
+  if (value === "true" || value === true || value === "1") {
+    return true;
+  }
+  if (value === "false" || value === false || value === "0") {
+    return false;
+  }
+  return false;
+};
+
+export const convertIntoInt = (value: string | number | null | undefined) => {
+  return value && !Number.isNaN(Number(value))
+    ? Math.round(Number(value))
+    : null;
+};
+
+export const convertIntoNumber = (
+  value: string | number | null | undefined
+) => {
+  if (typeof value === "string") {
+    value = value.replaceAll(",", "");
+  }
+  return value && !Number.isNaN(Number(value)) ? Number(value) : null;
+};
+
+export const setDateType = (value: string | Date | null | undefined) => {
+  if (value) {
+    if (!Number.isNaN(new Date(value).getTime())) {
+      return new Date(value);
+    }
+    return null;
+  }
+  return null;
+};
+
+export const downloadAttachmentFile = async ({
+  url,
+  fileName,
+}: {
+  url: string;
+  fileName: string;
+}) => {
+  const originalImage = `${url}`;
+  const linkTag = document.createElement("a");
+  linkTag.download = fileName;
+
+  const response = await axios(originalImage, {
+    headers: { "Cache-Control": "no-cache", Pragma: "no-cache", Expires: "0" },
+    responseType: "blob",
+  });
+
+  linkTag.href = URL.createObjectURL(response.data);
+  linkTag.click();
+};
+
+export const copyToClipboard = (value: string) => {
+  navigator.clipboard.writeText(value);
+};
+
+export const focusOnError = (
+  ref: MutableRefObject<Record<string, HTMLDivElement | null>>,
+  errors: FieldErrors<any> | undefined = {}
+) => {
+  const firstErrorKey = Object.keys(errors)?.[0];
+
+  if (firstErrorKey && Object.hasOwn(ref?.current, firstErrorKey)) {
+    ref?.current?.[firstErrorKey]?.scrollIntoView();
+  }
+};
