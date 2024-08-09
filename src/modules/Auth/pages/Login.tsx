1c114c1e4572d40419dc3beb106a347e83a0a876
ian-aguilar <josemelend.dev@gmail.com>
2024-08-09 19:12:39 +0530

    chanegs

--- src/modules/Auth/pages/Login.tsx
+++ src/modules/Auth/pages/Login.tsx
@@ -37,7 +37,7 @@ const Login = () => {
       email: values.email,
       password: values.password,
     });
-    if (data?.isVerified === false) {
+    if (data?.data?.isVerified === false) {
       navigate(RoutesPath.Otp, {
         state: {
           email: values.email,
