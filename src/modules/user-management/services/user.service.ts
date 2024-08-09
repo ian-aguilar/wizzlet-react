5583fd16ee56db4fe35d32f65e5e26ddbe2c8e75
Rupesh Prajapati <rupesh@crmtoolbox.io>
2024-08-09 16:55:33 +0530

    merge conflict resolve

--- src/modules/user-management/services/user.service.ts
+++ src/modules/user-management/services/user.service.ts
@@ -1,7 +1,12 @@
 // ** packages **
 
 // ** hooks **
-import { useAxiosGet, useAxiosPatch } from "@/hooks/useAxios";
+import {
+  useAxiosDelete,
+  useAxiosGet,
+  useAxiosPatch,
+  useAxiosPost,
+} from "@/hooks/useAxios";
 
 const AUTH_API_BASE_PATH = "/user";
 
@@ -17,7 +22,7 @@ export const useGetUserListAPI = () => {
   return { getUserListAPI, isLoading, isError, isSuccess };
 };
 
-//  ** Get All User List **
+//  ** User Status Change **
 export const useUserStatusChangeAPI = () => {
   // ** custom Hooks **
   const [callApi, { isLoading, isError, isSuccess }] = useAxiosPatch();
@@ -27,4 +32,28 @@ export const useUserStatusChangeAPI = () => {
   };
 
   return { userStatusChangeAPI, isLoading, isError, isSuccess };
+};
+
+//  ** Create User **
+export const useUserPostAPI = () => {
+  // ** custom Hooks **
+  const [callApi, { isLoading, isError, isSuccess }] = useAxiosPost();
+
+  const userPostAPI = async (data: object) => {
+    return callApi(`${AUTH_API_BASE_PATH}/create`, data);
+  };
+
+  return { userPostAPI, isLoading, isError, isSuccess };
+};
+
+//  ** Delete User **
+export const useUserDeleteAPI = () => {
+  // ** custom Hooks **
+  const [callApi, { isLoading, isError, isSuccess }] = useAxiosDelete();
+
+  const userDeleteAPI = async (id: number) => {
+    return callApi(`${AUTH_API_BASE_PATH}/remove/${id}`);
+  };
+
+  return { userDeleteAPI, isLoading, isError, isSuccess };
 };
