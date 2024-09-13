5c1ebe55a2e9482c789dc514c12c8ce06050964e
rajnikumar <rajnikumar.tank@esparkbizmail.com>
2024-09-13 20:28:26 +0530

    change in choose marketplace

--- src/components/form-fields/components/Checkbox.tsx
+++ src/components/form-fields/components/Checkbox.tsx
@@ -1,13 +1,6 @@
 import { ICheckboxProps } from "../types";
 
-const Checkbox = ({
-  name,
-  checkLabel,
-  isChecked,
-  onChange,
-  value,
-  mainClass,
-}: ICheckboxProps) => {
+const Checkbox = ({ name, checkLabel, isChecked, onChange, value, mainClass }: ICheckboxProps) => {
   return (
     <>
       <div className={` flex items-center ${mainClass} `}>
@@ -20,11 +13,11 @@ const Checkbox = ({
           type="checkbox"
           className="h-4 w-4 rounded-[2px] border-solid border-gray-300 accent-greenPrimary hover:ring-greenPrimary duration-300 transition-all  "
         />
-        <label
-          className="ml-2 text-darkText font-medium text-base leading-4 "
-          htmlFor={name}>
-          {checkLabel}
-        </label>
+        {checkLabel && (
+          <label className="ml-2 text-darkText font-medium text-base leading-4 " htmlFor={name}>
+            {checkLabel}
+          </label>
+        )}
       </div>
     </>
   );
