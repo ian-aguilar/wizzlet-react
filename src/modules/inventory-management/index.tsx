affecdeb6b6c6baed875a70b2067270642f1833e
ian-aguilar <josemelend.dev@gmail.com>
2024-10-04 16:06:32 +0530

    build error resolved

--- src/modules/inventory-management/index.tsx
+++ src/modules/inventory-management/index.tsx
@@ -2,7 +2,6 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { debounce } from "lodash";
-import { format } from "date-fns";
 
 // ** Icons **
 import {
