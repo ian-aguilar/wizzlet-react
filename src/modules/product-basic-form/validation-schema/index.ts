df471fb0e319790eb0ce31789dcd183190629a48
rajnikumar <rajnikumar.tank@esparkbizmail.com>
2024-10-22 19:46:29 +0530

    Bug resolving

--- src/modules/product-basic-form/validation-schema/index.ts
+++ src/modules/product-basic-form/validation-schema/index.ts
@@ -1,41 +1,47 @@
 import * as yup from "yup";
 
 export const productBasisFormValidationSchema = yup.object().shape({
-  title: yup.string().required("Title is required"),
-  description: yup.string().required("Description is required"),
+  title: yup.string().trim().required("Title is required"),
+  description: yup.string().trim().required("Description is required"),
   productType: yup
     .object()
     .shape({
-      value: yup.string().required(),
-      label: yup.string().required(),
+      value: yup.string().trim().required(),
+      label: yup.string().trim().required(),
     })
+    .typeError("Product Type is required")
     .required("Product Type is required"),
   image: yup.mixed().required("Image is required"),
   tagOptions: yup
     .array()
+    .min(1)
     .of(
       yup
         .object()
         .shape({
-          value: yup.string().required(),
-          label: yup.string().required(),
+          value: yup.string().trim().required(),
+          label: yup.string().trim().required(),
         })
         .required("required")
     )
+    .label("Tags")
     .required("Choose at least one"),
-  sku: yup.string().when("productType", {
-    is: (productType: { label: string; value: string }) =>
-      productType?.value === "NORMAL",
-    then: (schema) => schema.required("SKU is required"),
-    otherwise: (schema) => schema.notRequired(),
-  }),
-  quantity: yup.number().when("productType", {
+  sku: yup
+    .string()
+    .trim()
+    .when("productType", {
+      is: (productType: { label: string; value: string }) =>
+        productType?.value === "NORMAL",
+      then: (schema) => schema.required("SKU is required"),
+      otherwise: (schema) => schema.notRequired(),
+    }),
+  quantity: yup.number().min(0).when("productType", {
     is: (productType: { label: string; value: string }) =>
       productType?.value === "NORMAL",
     then: (schema) => schema.required("Quantity is required"),
     otherwise: (schema) => schema.notRequired(),
   }),
-  price: yup.number().when("productType", {
+  price: yup.number().min(0).when("productType", {
     is: (productType: { label: string; value: string }) =>
       productType?.value === "NORMAL",
     then: (schema) => schema.required("Price is required"),
